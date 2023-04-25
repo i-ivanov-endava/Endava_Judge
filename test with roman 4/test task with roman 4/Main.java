a2afa552f14576e4d598249487aa663d8e470ed0
nhWCpOyQBn <zsEqapR@oWYBsZK.com>
2023-04-25 11:54:20 +0000

    add Solution

--- test with roman 4/test task with roman 4/Main.java
+++ test with roman 4/test task with roman 4/Main.java
@@ -1,1 +1,1 @@
-import java.util.*; public class Main {public static void main(String[] args) {Scanner scanner = new Scanner(System.in);int number = scanner.nextInt();   Solution solution = new Solution();System.out.println(solution.isPalindrome(number));}}
+import java.util.Scanner;public class Main{public static void main(String[]args){Scanner in=new Scanner(System.in);int a=in.nextInt();int b=in.nextInt();System.out.println(a+b);}}
