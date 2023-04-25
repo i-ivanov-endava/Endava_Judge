754fa79217415ddd7dd9e82ab80f28be58ffd489
tQaNiCbySc <knrOmdH@LQvbysJ.com>
2023-04-25 20:48:39 +0300

    add Solution

--- test with roman 4/test task with roman 4/Main.java
+++ test with roman 4/test task with roman 4/Main.java
@@ -1,1 +1,1 @@
-import java.util.Scanner;public class Main{public static void main(String[]args){Scanner in=new Scanner(System.in);String[] input = in.nextLine().split(';');int a=Integer.parseInt(input[0]);int b=Integer.parseInt(input[1]);System.out.println(a+b);}}
+import java.util.Scanner;public class Main{public static void main(String[]args){Scanner in=new Scanner(System.in);String[] input = in.nextLine().split(";");int a=Integer.parseInt(input[0]);int b=Integer.parseInt(input[1]);System.out.println(a+b);}}
